c416b85ddc7ea5517d02c0bc5ca39ea61d9d7663
Dalston Ward <ward.dalston@gmail.com>
2023-04-08 14:52:33 -0500

    IsPalindrome
    Easy implementation and a recursive version with slicing.

--- scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/StretchProblems.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/StretchProblems.scala
@@ -1,11 +1,22 @@
 package com.labs1904.hwe.exercises
 
+import scala.annotation.tailrec
+
 object StretchProblems {
 
   /*
   Checks if a string is palindrome.
  */
-  def isPalindrome(s: String): Boolean = ???
+  def isPalindrome(s: String): Boolean = {
+//    s.reverse == s
+    if (s.head != s.last) {
+      return false
+    }
+    if (s.length >= 2) {
+      return true
+    }
+    isPalindrome(s.slice(1, s.length - 2))
+  }
 
   /*
 For a given number, return the next largest number that can be created by rearranging that number's digits.
