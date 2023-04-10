4c15549850219345047a831b46090f3a31bd92ff
Dalston Ward <ward.dalston@gmail.com>
2023-04-10 07:08:50 -0500

    solution to getNextBiggestNumber
    Uses recursion to avoid looping. Could probably be simplified:
    * don't reverse the sequence
    * combine the slice + windex where?
    * More graceful swapping
    
    Also it currently doesn't handle negative integers.

--- scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/StretchProblems.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/StretchProblems.scala
@@ -21,10 +21,37 @@ object StretchProblems {
   /*
 For a given number, return the next largest number that can be created by rearranging that number's digits.
 If no larger number can be created, return -1
- */
+   */
+def getNextBiggestInner(I: Seq[Int], stop: Int): Option[List[Int]] = {
+  if (stop == I.size) {
+    None
+  } else {
+    val swap: Int = I.slice(0, stop).indexWhere(_ > I(stop))
+    swap match {
+      case -1 => {
+        getNextBiggestInner(I, stop + 1)
+      }
+      case _ => {
+        Option(List(swap, stop))
+      }
+    }
+  }
+}
+
   def getNextBiggestNumber(i: Integer): Int = {
-    //TODO: Implement me!
-    0
+    if (i <= 11) {
+      return -1
+    }
+    val intSeq: Seq[Int] = i.toString.map((s: Char) => s.toString.toInt).reverse
+    val swap_exists = getNextBiggestInner(intSeq, 1)
+    swap_exists match {
+      case None => -1
+      case Some(swap) => {
+        val intSwapped = intSeq.updated(swap(0), intSeq(swap(1))).updated(swap(1), intSeq(swap(0)))
+        val (front, back) = intSwapped.splitAt(swap(1))
+        (front.sorted.reverse ++ back).reverse.mkString(sep="").toInt
+      }
+    }
   }
 
 }
