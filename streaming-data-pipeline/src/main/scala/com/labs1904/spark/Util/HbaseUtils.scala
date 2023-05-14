501686266ebc42f999ada0460f26282faa0c94e5
Dalston Ward <ward.dalston@gmail.com>
2023-05-14 11:29:28 -0500

    return connection
    We need to return a connection instead of a table so that we can close connection once we are done

--- streaming-data-pipeline/src/main/scala/com/labs1904/spark/Util/HbaseUtils.scala
+++ streaming-data-pipeline/src/main/scala/com/labs1904/spark/Util/HbaseUtils.scala
@@ -1,11 +1,11 @@
 package com.labs1904.spark.Util
 
-import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
-import org.apache.hadoop.hbase.client.{ConnectionFactory, Table}
+import org.apache.hadoop.hbase.{HBaseConfiguration}
+import org.apache.hadoop.hbase.client.{ConnectionFactory, Connection}
 object HbaseUtils {
-  def tableFromConnectionString(table: String, connection: String): Table = {
+  def newHBaseConnection(connection: String): Connection = {
     val conf = HBaseConfiguration.create()
     conf.set("hbase.zookeeper.quorum", connection)
-    ConnectionFactory.createConnection(conf).getTable(TableName.valueOf(table))
+    ConnectionFactory.createConnection(conf)
   }
 }
